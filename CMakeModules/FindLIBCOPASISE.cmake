b6dddad4efe08df1cecaed32aec74164cdbf0432
Brian Klahn <briandk@virginia.edu>
2020-01-29 16:35:02 -0500

    Installed and enabled (ran) pre-commit hook for automatically updating copyright and license.

--- CMakeModules/FindLIBCOPASISE.cmake
+++ CMakeModules/FindLIBCOPASISE.cmake
@@ -1,10 +1,17 @@
-# Copyright (C) 2018 by Pedro Mendes, Virginia Tech Intellectual 
-# Properties, Inc., University of Heidelberg, and University of 
+# BEGIN: Copyright 
+# Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the 
+# University of Virginia, University of Heidelberg, and University 
 # of Connecticut School of Medicine. 
 # All rights reserved. 
+# END: Copyright 
 
-
-
+# BEGIN: License 
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+#  
+#   http://www.apache.org/licenses/LICENSE-2.0 
+# END: License 
 
 # Locate library providing libcopasise
 # This module defines:
--- cpsapi-namespace.h
+++ cpsapi-namespace.h
@@ -1,7 +1,17 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
-// University of Virginia, University of Heidelberg, and University
-// Connecticut School of Medicine.
-// All rights reserved.
+// BEGIN: Copyright 
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
+// END: Copyright 
+
+// BEGIN: License 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//  
+//   http://www.apache.org/licenses/LICENSE-2.0 
+// END: License 
 
 #ifndef CPSAPI_NAMESPACE_H
 #define CPSAPI_NAMESPACE_H 1
