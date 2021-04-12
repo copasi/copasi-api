9cbeb3216c4d967d3a5feed4724ab16e44ed048e
Stefan Hoops <shoops@copasi.org>
2021-04-12 10:10:42 -0400

    Added Artistic License 2.0

--- tests/catch.hpp
+++ tests/catch.hpp
@@ -1,3 +1,17 @@
+// BEGIN: Copyright 
+// Copyright (C) 2021 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved 
+// END: Copyright 
+
+// BEGIN: License 
+// Licensed under the Artistic License 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//   https://opensource.org/licenses/Artistic-2.0 
+// END: License 
+
 /*
  *  Catch v1.5.6
  *  Generated: 2016-06-09 19:20:41.460328
